5b7ecd9a1b7845e95496af0b8c56f16c8091d63c
Immeleo <73342312+Immeleo@users.noreply.github.com>
2021-01-20 12:25:32 +0300

    Create behaviour.js

---
+++ behaviour.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+document.getElementById("smart_thumbnail");
+var thumbnailElement = document.getElementById("smart_thumbnail");
+thumbnailElement.addEventListener("click", function() {
+  var thumbnailElement = document.getElementById("smart_thumbnail");
+thumbnailElement.className;
+if (thumbnailElement.className == "small") {
+	thumbnailElement.className = "";
+}
+else{
+	thumbnailElement.className = "small";
+}
+});
+});
